8aef2632f1d04dd1b0c3880b1742ef8c888970ea
Amany-Ammar <amanymohmedamar@gmail.com>
2024-06-13 01:37:40 -0700

    first commit

---
+++ js/login.js
@@ -0,0 +1,21 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    var loginBtn = document.querySelector(".login-btn");
+    var inputWarning = document.querySelector(".input-warning");
+    var emailExist = document.querySelector(".email-exist");
+    var successfulSignup = document.querySelector(".successful-signup");
+    var loginEmail = document.getElementById("loginEmail");
+    var loginPassword = document.getElementById("loginPassword");
+
+    loginBtn.addEventListener("click", function() {
+        if (loginEmail.value.trim() === "" || loginPassword.value.trim() === "") {
+            inputWarning.classList.remove("d-none");
+            emailExist.classList.add("d-none");
+            successfulSignup.classList.add("d-none");
+        } else {
+            var helloPage = "./hello.html";
+
+            window.location.href = helloPage;
+        }
+    });
+});
